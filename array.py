1a7bfbb85758178a842f49ac2c7fabb572d594cf
KhushiSingla-tech <73333739+KhushiSingla-tech@users.noreply.github.com>
2021-04-27 09:27:12 +0530

    array.py

---
+++ array.py
@@ -0,0 +1,7 @@
+arr = [1,2,3,4,5]
+print(arr)
+print("first element: " , arr[0])
+print("second element: " , arr[1])
+print("third element: " , arr[2])
+print("fourth element: " , arr[3])
+print("fifth element: " , arr[4])
